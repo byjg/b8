04a9b66385b287246dcb7c674d1a7ccae5bcd89c
Greg Boggs <greg.boggs@thinkshout.com>
2017-05-14 15:20:49 -0700

    fiddle

--- example/index.php
+++ example/index.php
@@ -19,8 +19,7 @@ namespace Example;
 #   along with this program; if not, write to the Free Software Foundation,
 #   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-use B8;
-
+use B8\B8;
 ##### This is an example script demonstrating how b8 can be used. #####
 
 /*
